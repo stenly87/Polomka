b44a848dbf2132facb7e40afaa382634c2b9ca4f
ppk <ppk_student@hotmail.com>
2021-10-05 09:34:50 +1000

    Расширение для клиента

---
+++ Поломка/Extension/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Поломка.db
+{ 
+    public partial class Client
+    {
+        public int CountVisit { get => ClientService.Count; }
+
+        public DateTime? LastVisit { 
+            get {
+                if (ClientService.Count() > 0)
+                    return ClientService.Max(x => x.StartTime);
+                else
+                    return null;
+            } 
+        }
+    }
+}
--- Поломка/ViewModels/ListClientsViewModel.cs
+++ Поломка/ViewModels/ListClientsViewModel.cs
@@ -123,6 +123,15 @@ namespace Поломка.ViewModels
                 Pagination();
             });
 
+            ViewClientWithBirthdayThisMonth = new CustomCommand(()=>
+            {
+                searchResult = DBInstance.Get().Client
+                    .Where(c => c.Birthday.HasValue &&
+                        c.Birthday.Value.Month == DateTime.Now.Month).ToList();
+                InitPagination();
+                Pagination();
+            });
+
             Search();
         }
 
@@ -131,22 +140,27 @@ namespace Поломка.ViewModels
             var search = SearchText.ToLower();
             if (SelectedSearchType == "ФИО")
                 searchResult = DBInstance.Get().Client
-                    .Where(c => c.GenderCode.Contains(SelectedGenderFilter.Code) && 
-                    ( c.FirstName.ToLower().Contains(search) ||
+                    .Where(c => c.GenderCode.Contains(SelectedGenderFilter.Code) &&
+                    (c.FirstName.ToLower().Contains(search) ||
                     c.LastName.ToLower().Contains(search) ||
                     c.Patronymic.ToLower().Contains(search)
                     )).ToList();
             else if (SelectedSearchType == "Email")
                 searchResult = DBInstance.Get().Client
-                    .Where(c => c.GenderCode.Contains(SelectedGenderFilter.Code) && 
+                    .Where(c => c.GenderCode.Contains(SelectedGenderFilter.Code) &&
                     c.Email.ToLower().Contains(search)).ToList();
             else if (SelectedSearchType == "Телефон")
                 searchResult = DBInstance.Get().Client
-                    .Where(c => c.GenderCode.Contains(SelectedGenderFilter.Code) && 
+                    .Where(c => c.GenderCode.Contains(SelectedGenderFilter.Code) &&
                     c.Phone.ToLower().Contains(search)).ToList();
+            InitPagination();
+            Pagination();
+        }
+
+        private void InitPagination()
+        {
             SearchCountRows = $"Найдено записей: {searchResult.Count} из {DBInstance.Get().Client.Count()}";
             paginationPageIndex = 0;
-            Pagination();
         }
 
         private void Pagination()
